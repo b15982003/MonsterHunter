c92a20b97856a694c92388ebe4b393a1bf7fd269
Ray Chang <b15982003@gmail.com>
2020-07-26 20:07:20 +0800

    crawling detail UI

--- app/src/main/java/com/ray/monsterhunter/crawlingdetail/CrawlingDetail.kt
+++ app/src/main/java/com/ray/monsterhunter/crawlingdetail/CrawlingDetail.kt
@@ -11,9 +11,6 @@ import com.ray.monsterhunter.R
 
 class CrawlingDetail : Fragment() {
 
-    companion object {
-        fun newInstance() = CrawlingDetail()
-    }
 
     private lateinit var viewModel: CrawlingDetailViewModel
 
@@ -22,12 +19,6 @@ class CrawlingDetail : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.crawling_detail_fragment, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(CrawlingDetailViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 
 }
